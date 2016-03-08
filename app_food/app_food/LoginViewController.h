8db30feb98ddd1de5e779b17c1c87196c79843c8
蒋豪 <653513754@qq.com>
2016-03-08 15:16:24 +0800

    完成登陆验证功能

---
+++ app_food/app_food/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  app_food
+//
+//  Created by 蒋豪 on 16/2/24.
+//  Copyright © 2016年 jh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ app_food/app_food/LoginViewController.m
@@ -0,0 +1,73 @@
+//
+//  LoginViewController.m
+//  app_food
+//
+//  Created by 蒋豪 on 16/2/24.
+//  Copyright © 2016年 jh. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "AFNetworking.h"
+#import "MBProgressHUD.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *pwTextField;
+@property (weak, nonatomic) IBOutlet UILabel *userError;
+@property (weak, nonatomic) IBOutlet UILabel *pwError;
+
+@end
+
+@implementation LoginViewController
+- (IBAction)loginAction:(id)sender {
+    self.userError.hidden=YES;
+    self.pwError.hidden=YES;
+    NSString *userName=self.userNameTextField.text;
+    NSString *passWord=self.pwTextField.text;
+    if ([userName isEqualToString:@""]) {
+        self.userError.hidden=NO;
+        return;
+    }else if([passWord isEqualToString:@""]){
+        self.pwError.hidden=NO;
+        return;
+    }
+    
+    NSString *url=@"http://127.0.0.1:8080/app_food/UserLoginServlet";
+    NSDictionary *param=@{@"userName":userName,
+                          @"password":passWord,
+                          };
+    AFHTTPSessionManager *manage=[AFHTTPSessionManager manager];
+    [manage GET:url parameters:param progress:^(NSProgress * _Nonnull downloadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"success,%@",responseObject);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error,%@",error);
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.logoImageView.image=[UIImage imageNamed:@"logo"];
+   
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
